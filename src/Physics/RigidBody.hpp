1393e6845ceb1ccccf3ece98978b2d872de58359
etc597 <etc597@gmail.com>
2018-01-26 00:27:11 -0800

    Removed jank shit for now, working on arch first

--- src/Physics/RigidBody.hpp
+++ src/Physics/RigidBody.hpp
@@ -6,42 +6,16 @@
 #include <glm/glm.hpp>
 
 class Object;
-
-struct PhysicsData
-{
-  PhysicsData();
-  PhysicsData(glm::vec3 position, glm::vec3 velocity, glm::vec3 acceleration, glm::vec3 impulse);
-  glm::vec3 mPosition;
-  glm::vec3 mVelocity;
-  glm::vec3 mAcceleration;
-};
-
-struct RigidBodyData
-{
-  PhysicsData mLinear;
-  PhysicsData mAngular;
-  float mMass = 1.0f;
-};
-
-struct AppliedForce
-{
-  glm::vec3 mPosition;
-  glm::vec3 mForce;
-};
+class Model;
 
 class RigidBody
 {
 public:
   RigidBody();
-  void Init(Object * object, RigidBodyData data = RigidBodyData());
+  void Init(Object * object);
   void Update(float dt);
 private:
-  glm::quat qDotDot(glm::quat q, glm::quat qDot, glm::vec3 torque);
-  PhysicsData mLinear;
-  PhysicsData mAngular;
-  glm::mat3 mInertiaTensor;
-  std::vector<AppliedForce> mForces;
-  float mMass;
+  Model * mModel;
 };
 
 #endif
