2086916354fa88b4642be465c2b3942494ba2416
etc597 <etc597@gmail.com>
2018-02-19 17:25:02 -0800

    Working on aabb tree

---
+++ src/Physics/AABBTree.cpp
@@ -0,0 +1,44 @@
+#include "AABBTree.hpp"
+
+void AABBTree::InsertData(Collider * aCollider)
+{
+}
+
+void AABBTree::UpdateData(Collider * aCollider)
+{
+}
+
+void AABBTree::RemoveData(Collider * aCollider)
+{
+}
+
+void AABBTree::SelfQuery(QueryResults & results)
+{
+}
+
+AABBTree::Node * AABBTree::GetLeft(Node * aNode)
+{
+  // TODO: Make this safe
+  return &mNodes[aNode->mLeftIndex];
+}
+
+AABBTree::Node * AABBTree::GetRight(Node * aNode)
+{
+  // TODO: Make this safe
+  return &mNodes[aNode->mRightIndex];
+}
+
+AABBTree::Node * AABBTree::GetParent(Node * aNode)
+{
+  // TODO: Make this safe
+  return &mNodes[aNode->mParentIndex];
+}
+
+AABBTree::Node * AABBTree::GetSibling(Node * aNode)
+{
+  Node * mParent = GetParent(aNode);
+  if (mParent->mLeftIndex = aNode->mSelfIndex) {
+    return GetRight(mParent);
+  }
+  return GetLeft(mParent);
+}
--- src/Physics/AABBTree.hpp
+++ src/Physics/AABBTree.hpp
@@ -1,10 +1,47 @@
 #ifndef Physics_AABBTree_hpp
 #define Physics_AABBTree_hpp
 
+#include "Physics/AABB.hpp"
+#include <stack>
+
+class Collider;
+
+class QueryResults
+{
+public:
+  std::vector<std::pair<Collider*, Collider*>> mPairs;
+};
+
 class AABBTree
 {
 public:
   AABBTree() = default;
+
+  void InsertData(Collider * aCollider);
+  void UpdateData(Collider * aCollider);
+  void RemoveData(Collider * aCollider);
+
+  void SelfQuery(QueryResults& results);
+private:
+  class Node
+  {
+  public:
+    Collider * mCollider;
+    AABB mAABB;
+    int mParentIndex;
+    int mLeftIndex;
+    int mRightIndex;
+    int mSelfIndex;
+  };
+
+  Node * GetLeft(Node * aNode);
+  Node * GetRight(Node * aNode);
+  Node * GetParent(Node * aNode);
+  Node * GetSibling(Node * aNode);
+
+  Node * mRoot;
+  std::vector<Node> mNodes;
+  std::stack<int> mFreeIndex;
 };
 
 #endif
